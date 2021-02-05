352c12d6e51a7233f255dd262c9bc9f656098056
Taylor Lea <taylorl@jfrog.com>
2021-02-04 16:39:42 -0800

    Add storyboad and ViewController code

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -14,6 +14,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showMessage(sender: UIButton) {
+        let title = NSAttributedString(string: "Hello", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        let message = NSAttributedString(string: "World", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        let alertController = UIAlertController(title: "", message: "", preferredStyle: UIAlertController.Style.alert)
+        alertController.setValue(message, forKey: "attributedMessage")
+        alertController.setValue(title, forKey: "attributedTitle")
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+        let subview = (alertController.view.subviews.first?.subviews.first?.subviews.first!)! as UIView
+        let JFROGGreen = UIColor(red: (65/255.0), green: (191/255.0), blue: (71/255.0), alpha: 1.0)
+        alertController.view.tintColor = UIColor.white
+    
+        subview.backgroundColor = JFROGGreen
+    }
 
 }
-
---
+++ README.md
@@ -0,0 +1,4 @@
+# iOS Hello World
+
+A sample iOS app to demo Pipelines iOS builds on Mac buildplane nodes.
+
